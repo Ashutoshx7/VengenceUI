3a0a3fea1d8d36120f00abdf4225d9d607f64539
Ashutoshx7 <as1142120@gmail.com>
2026-05-27 17:55:27 +0530

    remove: Delete Beam Tunnel component entirely from library

--- src/components/docs/beam-tunnel-demo.tsx
+++
@@ -1,9 +0,0 @@
-import { BeamTunnel } from "@/components/ui/beam-tunnel";
-
-export function BeamTunnelDemo() {
-  return (
-    <BeamTunnel className="w-full h-full min-h-[500px]" />
-  );
-}
-
-export default BeamTunnelDemo;
